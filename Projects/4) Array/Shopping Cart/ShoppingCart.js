28600f2eeecc102a96273d5cf324796184041474
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-01-02 18:12:45 +0545

    JavaScript Array Project 5

--- Projects/4) Array/Shopping Cart/ShoppingCart.js
+++ Projects/4) Array/Shopping Cart/ShoppingCart.js
@@ -3,11 +3,13 @@ const productNameEL = document.getElementById("product-name");
 const productQuantityEL = document.getElementById("product-quantity");
 const productPriceEL = document.getElementById("product-price");
 const message = document.getElementById("message");
+const list = document.getElementById("list");
+const total = document.getElementById("total");
 function addCart() {
   const productName = productNameEL.value.trim();
   const productQuantity = parseInt(productQuantityEL.value);
   const productPrice = parseInt(productPriceEL.value);
-  if (productName === "" || isNaN(productPrice)) {
+  if (productName === "" || isNaN(productPrice) || !productQuantity) {
     message.textContent = "Enter Your Cart Information";
     message.style.color = "red";
     message.style.opacity = "1";
@@ -39,4 +41,19 @@ function addCart() {
   productPriceEL.value = "";
   productNameEL.value = "";
   productQuantityEL.value = "";
+  productNameEL.focus();
+}
+
+function display() {
+  list.innerHTML = "";
+  let totalPrice = 0;
+  productList.forEach((value) => {
+    const li = document.createElement("li");
+    li.textContent = `${value.productName} - ${
+      value.productPrice * value.productQuantity
+    }`;
+    list.appendChild(li);
+    totalPrice += value.productPrice * value.productQuantity;
+  });
+  total.textContent = `${totalPrice}`;
 }
