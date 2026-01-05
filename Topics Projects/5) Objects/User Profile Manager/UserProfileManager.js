6cbecf99b747d6f4701cb4b899cd018818977915
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-01-05 18:18:29 +0545

    UPM User Add functionality

--- Topics Projects/5) Objects/User Profile Manager/UserProfileManager.js
+++ Topics Projects/5) Objects/User Profile Manager/UserProfileManager.js
@@ -1,1 +1,32 @@
+const users = [
+  { name: "Anuj", email: "anuj@gmail.com", age: 22 },
+  { name: "Ram", email: "ram@gmail.com", age: 25 },
+  { name: "Sita", email: "sita@gmail.com", age: 21 },
+  { name: "Hari", email: "hari@gmail.com", age: 27 },
+  { name: "Gita", email: "gita@gmail.com", age: 23 },
+];
+
+const usernameEL = document.getElementById("user");
+const emailEL = document.getElementById("email");
+const ageEL = document.getElementById("age");
+const resultList = document.getElementById("resultList");
+
+function add() {
+  const name = usernameEL.value.trim();
+  const email = emailEL.value.trim();
+  const age = Number(ageEL.value);
+  const exist = users.some((user) => user.email === email);
+  if (!name || !email || !age) {
+    alert("Fill All Fields");
+    return;
+  }
+  if (exist) {
+    alert("Details with this info exist");
+    return;
+  }
+  users.push({ name, email, age });
+  document.querySelectorAll("#user,#email,#age").forEach((v) => {
+    v.value = "";
+  });
+}
 
