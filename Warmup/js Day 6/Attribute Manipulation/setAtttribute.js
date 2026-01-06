be0a1e13c2389c88f2058c9948d8e5d92fc50b7b
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-01-06 19:16:49 +0545

    Attribute Manipulation

---
+++ Warmup/js Day 6/Attribute Manipulation/setAtttribute.js
@@ -0,0 +1,9 @@
+const anchor = document.querySelector("a");
+console.log(anchor);
+anchor.href = "https://www.youtube.com";
+
+const img = document.querySelector("img");
+img.setAttribute(
+  "src",
+  "https://www.onlinekhabar.com/wp-content/uploads/2019/06/KUMARI-Nihira-Bajracharya-2.jpg"
+);
