73392dff23dfaec8f657987d40c63adfa4a53443
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-01-06 18:05:37 +0545

    DOM Text Manipulation

---
+++ Warmup/js Day 6/DOM Text Manipulate/DOMTextManipulate.js
@@ -0,0 +1,4 @@
+document.getElementById("p").style.color = "red";
+document.querySelector("h1").textContent = "DOM Text Manipulation";
+document.querySelector("div").innerHTML =
+  "<h1>Inserting HTML Element Through DOM <em>innerHtml</em></h1>";
