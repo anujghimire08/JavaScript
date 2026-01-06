100890e28de7bffdceb18b21c13ab9c5d0e0503e
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-01-06 17:37:53 +0545

    DOM element selector

---
+++ Warmup/js Day 6/DOM.js
@@ -0,0 +1,11 @@
+const h1 = document.getElementById("h1");
+console.dir(h1);
+
+const spans = document.getElementsByClassName("spans");
+console.log(spans);
+
+const p = document.querySelector("p");
+console.log(p);
+
+const divs = document.querySelectorAll("div");
+console.log(divs);
