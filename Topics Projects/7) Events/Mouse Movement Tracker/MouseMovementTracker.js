c3e1f8a5be8ff50633f0609a9209aa54aed1d17f
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-01-15 14:07:11 +0545

    cursor center mouse event

--- Topics Projects/7) Events/Mouse Movement Tracker/MouseMovementTracker.js
+++ Topics Projects/7) Events/Mouse Movement Tracker/MouseMovementTracker.js
@@ -1,6 +1,6 @@
 const box = document.querySelector(".box");
 
 window.addEventListener("mousemove", (info) => {
-  box.style.top = `${info.clientY}px`;
-  box.style.left = `${info.clientX}px`;
+  box.style.top = `${info.clientY - box.offsetHeight / 2}px`;
+  box.style.left = `${info.clientX - box.offsetWidth / 2}px`;
 });
