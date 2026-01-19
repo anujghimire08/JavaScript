40c4ccb1e2e52aec16d4c081fadeb00b48d15e79
Anuj Ghimire <contact.anujghimire@gmail.com>
2026-01-19 09:38:54 +0545

    closure as obj

--- Warmup/js Day 8/closure.js
+++ Warmup/js Day 8/closure.js
@@ -1,3 +1,15 @@
+function outer() {
+  let x = 10;
+
+  function inner() {
+    console.log(x);
+  }
+
+  inner();
+}
+
+// outer();
+
 function abcd() {
   let count = 0;
   return () => {
@@ -9,15 +21,62 @@ function abcd() {
     }
   };
 }
-const fun = abcd();
-fun();
-fun();
-fun();
+// const fun = abcd();
+// fun();
+// fun();
+// fun();
 
+// const fun2 = abcd();
+// fun2();
+// fun2();
+// fun2();
+// fun2();
 
+//Real Use Case
+function pass() {
+  let passcode = 12345;
 
-const fun2 = abcd();
-fun2();
-fun2();
-fun2();
-fun2();
+  return function check(userinput) {
+    return passcode === userinput;
+  };
+}
+
+let login = pass();
+// console.log(login(1234));
+
+// Returning Closures as Objects
+function bank() {
+  let amount = 120000;
+  return {
+    // Old / Traditional Syntax
+    deposit: function (blc) {
+      amount += blc;
+      return amount;
+    },
+    withdraw: function (blc) {
+      amount -= blc;
+      return amount;
+    },
+  };
+}
+let fun = bank();
+// console.log(fun.deposit(10000));
+// console.log(fun.withdraw(20000));
+
+function bank() {
+  let amount = 120000;
+  return {
+    // Modern / Shorthand Syntax
+    deposit(blc) {
+      amount += blc;
+      return amount;
+    },
+    withdraw(blc) {
+      amount -= blc;
+      return amount;
+    },
+  };
+}
+
+let acc = bank();
+// console.log(acc.withdraw(100000));
